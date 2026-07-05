e47ad85eff9570c7392b2b3cec8c39d049ab8608
Mohamed Fazil <mohamedfazil152@gmail.com>
2026-07-05 22:41:00 +0530

    user class added

--- Main.java
+++ Main.java
@@ -4,20 +4,20 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 //       Product
-        List<Product> products=new ArrayList<Product>(
-                List.of(
-                        new Product("Pixel 10 XL", 112999, 10),
-                        new Product("iPhone 15 Pro", 129999, 5),
-                        new Product("Samsung S24 Ultra", 109999, 8),
-                        new Product("OnePlus 12", 64999, 15),
-                        new Product("Xiaomi 14 Pro", 59999, 20),
-                        new Product("Nothing Phone 2", 39999, 25),
-                        new Product("Realme GT 6", 34999, 30),
-                        new Product("Vivo X100", 89999, 12),
-                        new Product("Oppo Find X7", 94999, 7),
-                        new Product("Motorola Edge 50", 27999, 18)
-                )
-        );
+//        List<Product> products=new ArrayList<Product>(
+//                List.of(
+//                        new Product("Pixel 10 XL", 112999, 10),
+//                        new Product("iPhone 15 Pro", 129999, 5),
+//                        new Product("Samsung S24 Ultra", 109999, 8),
+//                        new Product("OnePlus 12", 64999, 15),
+//                        new Product("Xiaomi 14 Pro", 59999, 20),
+//                        new Product("Nothing Phone 2", 39999, 25),
+//                        new Product("Realme GT 6", 34999, 30),
+//                        new Product("Vivo X100", 89999, 12),
+//                        new Product("Oppo Find X7", 94999, 7),
+//                        new Product("Motorola Edge 50", 27999, 18)
+//                )
+//        );
 
 //        products.forEach(p-> System.out.println(p.getName()+" "+p.getPrice()));
 //        products.stream().filter(p->p.getPrice()>70000).forEach(p-> System.out.println(p.getPrice()));
